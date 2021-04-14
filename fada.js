c0c18749462aae5f0267e2e73f4d8d4e602827da
Isac sabino <isacsabinooficial@gmail.com>
2021-04-14 18:07:08 -0300

    fada.js
    fada

--- fada.js
+++ fada.js
@@ -2,7 +2,7 @@
 
 function fadaDaVerdade() {
     elem = document.querySelector('#pagFase');
-    elem.style.backgroundImage = "url('/img/fada/fada1.png')";
+    elem.style.backgroundImage = "url('./img/fada/fada1.png')";
     setTimeout(function () {
         escolha1 = alert("Clique em ok para avançar.");
         fadaFaseincial()
@@ -11,7 +11,7 @@ function fadaDaVerdade() {
 // Começo do jogo.
 function fadaFaseincial() {
     elem = document.querySelector('#pagFase');
-    elem.style.backgroundImage = "url('/img/fada/fase1.png')";
+    elem.style.backgroundImage = "url('./img/fada/fase1.png')";
     setTimeout(function () {
         escolha1 = prompt("Escolha o seu caminho.");
         if (escolha1 == 1) {
@@ -35,7 +35,7 @@ function fadaFaseincial() {
 // Inicio da Fase 1 
 function fadaFase0b() {
     elem = document.querySelector('#pagFase');
-    elem.style.backgroundImage = "url('/img/fada/fase1.0.png')";
+    elem.style.backgroundImage = "url('./img/fada/fase1.0.png')";
     setTimeout(function () {
         escolha1 = prompt("Escolha uma das opções.");
         if (escolha1 == 1) {
@@ -54,7 +54,7 @@ function fadaFase0b() {
 // Caminho vitoria / fase 1
 function fadafase1b() {
     elem = document.querySelector('#pagFase');
-    elem.style.backgroundImage = "url('/img/fada/fase4.png')";
+    elem.style.backgroundImage = "url('./img/fada/fase4.png')";
     setTimeout(function () {
         setTimeout(function () {
             escolha1 = alert("Avançar");
@@ -66,7 +66,7 @@ function fadafase1b() {
 }
 function fadafase1c() {
     elem = document.querySelector('#pagFase');
-    elem.style.backgroundImage = "url('/img/fada/fase4.0.png')";
+    elem.style.backgroundImage = "url('./img/fada/fase4.0.png')";
     setTimeout(function () {
         setTimeout(function () {
             escolha1 = alert("Você precisa derrotar os Reptilianos para poder desativar a bomba, Clique em Ok 20 vezes para derrotá-los.");
@@ -78,7 +78,7 @@ function fadafase1c() {
 }
 function fadafase1d() {
     elem = document.querySelector('#pagFase');
-    elem.style.backgroundImage = "url('/img/fada/fase4.0.png')";
+    elem.style.backgroundImage = "url('./img/fada/fase4.0.png')";
     setTimeout(function () {
         setTimeout(function () {
             combatemagia()
@@ -89,7 +89,7 @@ function fadafase1d() {
 }
 function combatemagia() {
     elem = document.querySelector('#pagFase');
-    elem.style.backgroundImage = "url('/img/fada/fase4.1.png')";
+    elem.style.backgroundImage = "url('./img/fada/fase4.1.png')";
     setTimeout(function () {
         setTimeout(function () {
             while (educar < 20) {
@@ -104,7 +104,7 @@ function combatemagia() {
 }
 function fadafase1vitoria() {
     elem = document.querySelector('#pagFase');
-    elem.style.backgroundImage = "url('/img/fada/vitoriafase4.png')";
+    elem.style.backgroundImage = "url('./img/fada/vitoriafase4.png')";
     setTimeout(function () {
         setTimeout(function () {
             escolha1 = alert("Parabéns, você conseguiu salvar o mundo!!");
@@ -119,7 +119,7 @@ function fadafase1vitoria() {
 // Caminho gamer over. / fase 1
 function fadagameover1() {
     elem = document.querySelector('#pagFase');
-    elem.style.backgroundImage = "url('/img/fada/gameoverfada.png')";
+    elem.style.backgroundImage = "url('./img/fada/gameoverfada.png')";
     setTimeout(function () {
         escolha1 = alert("Você morreu, Aperte f5 para voltar.");
                 location.reload();
@@ -134,7 +134,7 @@ function fadagameover1() {
 // Inicio da Fase 2 ////////
 function fadaFase2() {
     elem = document.querySelector('#pagFase');
-    elem.style.backgroundImage = "url('/img/fada/fase2.png')";
+    elem.style.backgroundImage = "url('./img/fada/fase2.png')";
     setTimeout(function () {
         escolha1 = prompt("Escolha uma das opções.");
         if (escolha1 == 1) {
@@ -153,7 +153,7 @@ function fadaFase2() {
 // Caminho vitoria / fase 2
 function fadafase2b() {
     elem = document.querySelector('#pagFase');
-    elem.style.backgroundImage = "url('/img/fada/fase4.png')";
+    elem.style.backgroundImage = "url('./img/fada/fase4.png')";
     setTimeout(function () {
         setTimeout(function () {
             escolha1 = alert("Avançar");
@@ -165,7 +165,7 @@ function fadafase2b() {
 }
 function fadafase2c() {
     elem = document.querySelector('#pagFase');
-    elem.style.backgroundImage = "url('/img/fada/fase4.0.png')";
+    elem.style.backgroundImage = "url('./img/fada/fase4.0.png')";
     setTimeout(function () {
         setTimeout(function () {
             escolha1 = alert("Você precisa derrotar os Reptilianos para poder desativar a bomba, Clique em Ok 20 vezes para derrotá-los.");
@@ -177,7 +177,7 @@ function fadafase2c() {
 }
 function fadafase2d() {
     elem = document.querySelector('#pagFase');
-    elem.style.backgroundImage = "url('/img/fada/fase4.0.png')";
+    elem.style.backgroundImage = "url('./img/fada/fase4.0.png')";
     setTimeout(function () {
         setTimeout(function () {
             fadacombate()
@@ -188,7 +188,7 @@ function fadafase2d() {
 }
 function fadacombate() {
     elem = document.querySelector('#pagFase');
-    elem.style.backgroundImage = "url('/img/fada/fase4.1.png')";
+    elem.style.backgroundImage = "url('./img/fada/fase4.1.png')";
     setTimeout(function () {
         setTimeout(function () {
             while (educar < 20) {
@@ -203,7 +203,7 @@ function fadacombate() {
 }
 function fadafase2vitoria() {
     elem = document.querySelector('#pagFase');
-    elem.style.backgroundImage = "url('/img/fada/vitoriafase4.png')";
+    elem.style.backgroundImage = "url('./img/fada/vitoriafase4.png')";
     setTimeout(function () {
         setTimeout(function () {
             alert("Você salvou o mundo.");
@@ -216,7 +216,7 @@ function fadafase2vitoria() {
 // Caminho gamer over. / fase 2 - pronto
 function fadafase2a() {
     elem = document.querySelector('#pagFase');
-    elem.style.backgroundImage = "url('/img/fada/fase2.0.png')";
+    elem.style.backgroundImage = "url('./img/fada/fase2.0.png')";
     setTimeout(function () {
         setTimeout(function () {
             fadacombate2a()
@@ -227,7 +227,7 @@ function fadafase2a() {
 }
 function fadacombate2a() {
     elem = document.querySelector('#pagFase');
-    elem.style.backgroundImage = "url('/img/fada/fase2.0.png')";
+    elem.style.backgroundImage = "url('./img/fada/fase2.0.png')";
     setTimeout(function () {
         setTimeout(function () {
             while (educar < 3) {
@@ -241,7 +241,7 @@ function fadacombate2a() {
 }
 function fadagameover2a() {
     elem = document.querySelector('#pagFase');
-    elem.style.backgroundImage = "url('/img/fada/fase2.1.png')";
+    elem.style.backgroundImage = "url('./img/fada/fase2.1.png')";
     setTimeout(function () {
         escolha1 = alert("Você matou o lobo, ele te ajudaria a salvar o mundo. Aperte f5 para voltar.");
         location.reload();
